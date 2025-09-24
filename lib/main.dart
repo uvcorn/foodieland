8ddbf61a84e9a6e37fe2fba2d07a033db4629c7b
uvcorn <mdshadhinali.official@gmail.com>
2025-09-24 12:43:52 +0600

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:foodieland/core/app.dart';
